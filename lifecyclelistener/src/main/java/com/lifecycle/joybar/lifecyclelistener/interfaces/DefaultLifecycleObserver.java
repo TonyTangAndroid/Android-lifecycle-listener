fce165ab42de755cec3a44bbc791f504c191d6a2
TonyTandAndroid <tonythompsoncmu@gmail.com>
2020-07-11 21:27:58 -0700

    Extends LifecycleObserver

--- lifecyclelistener/src/main/java/com/lifecycle/joybar/lifecyclelistener/interfaces/DefaultLifecycleObserver.java
+++ lifecyclelistener/src/main/java/com/lifecycle/joybar/lifecyclelistener/interfaces/DefaultLifecycleObserver.java
@@ -1,9 +1,10 @@
 package com.lifecycle.joybar.lifecyclelistener.interfaces;
 
+import androidx.lifecycle.LifecycleObserver;
 import androidx.lifecycle.LifecycleOwner;
 
 /** Created by joybar on 2017/6/29. */
-public interface DefaultLifecycleObserver {
+public interface DefaultLifecycleObserver extends LifecycleObserver {
 
   void onStart(LifecycleOwner lifecycleOwner);
 
